83d6ba493aa1fb1a05d8f1d035909c95ce8b548e
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-16 21:31:09 +0200

    Correction #8

--- 1-10/22_NSI_08/22-NSI-08.py
+++ 1-10/22_NSI_08/22-NSI-08.py
@@ -1,9 +1,17 @@
+def recherche(elt,tab):
+  for i in range(len(tab)):
+    if tab[i] == elt:
+      return i 
+  return -1 
+
+print(recherche(1, [2, 3, 4,1]))
+
 def insere(a, tab):
     l = list(tab) #l contient les mêmes éléments que tab
     l.append(a)
-    i = ...
-    while a < ... and i >= 0: 
-      l[i+1] = ...
+    i = len(l) - 2 
+    while a < l[i] and i >= 0: 
+      l[i+1] = l[i]
       l[i] = a
-      i = ...
+      i = i - 1 
     return l
