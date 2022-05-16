4cc75fb5c4abca920e7a45294c2fbb29ff834b6e
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-16 20:41:06 +0200

    Add files via upload

--- 1-10/22_NSI_01/22-NSI-01.py
+++ 1-10/22_NSI_01/22-NSI-01.py
@@ -1,10 +1,18 @@
+def recherche(caractere, mot):
+    occ = 0 
+    for i in mot: 
+        if i == caractere:
+            occ += 1 
+    return occ 
+
+
 Pieces = [100,50,20,10,5,2,1]
 def rendu_glouton(arendre, solution=[], i=0):
-       if arendre == 0:
-       return ...
-    p = pieces[i]
-    if p <= ... :
-        solution.append(...)
+    if arendre == 0:
+        return solution
+    p = Pieces[i]
+    if p <= arendre :
+        solution.append(p)
         return rendu_glouton(arendre - p, solution, i)
     else :
-        return rendu_glouton(arendre, solution, ...)
+        return rendu_glouton(arendre, solution, i+1)
