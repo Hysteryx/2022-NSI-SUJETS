182036952b9d75ebef452ddad96c2823225f46d5
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-16 21:42:33 +0200

    Correction #9

--- 1-10/22_NSI_09/22-NSI-09.py
+++ 1-10/22_NSI_09/22-NSI-09.py
@@ -1,3 +1,16 @@
+def calcul(n):
+    res = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n //2 
+        else:
+            n = 3*n + 1 
+        res.append(n)
+    return res 
+        
+print(calcul(7))
+
+
 dico = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, \
         "H":8, "I":9, "J":10, "K":11, "L":12, "M":13, \
         "N":14, "O":15, "P":16, "Q":17, "R":18, "S":19, \
@@ -6,12 +19,12 @@ dico = {"A":1, "B":2, "C":3, "D":4, "E":5, "F":6, "G":7, \
 def est_parfait(mot) :
     #mot est une chaîne de caractères (en lettres majuscules)
     code_c = ""
-    code_a = ???
+    code_a = 0
     for c in mot :
-        code_c = code_c + ???
-        code_a = ???
+        code_c = code_c + str(dico[c])
+        code_a += dico[c] 
     code_c = int(code_c)
-    if ??? :
+    if code_c % code_a == 0:
         mot_est_parfait = True
     else :
         mot_est_parfait = False
