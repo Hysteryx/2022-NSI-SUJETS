ac8d17dbadebb0ea2acb3daf98085459174c04bc
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-16 21:35:44 +0200

    Correction 7

--- 1-10/22_NSI_07/22-NSI-07.py
+++ 1-10/22_NSI_07/22-NSI-07.py
@@ -1,9 +1,22 @@
+def conv_bin(n):
+    if n == 0:
+        return ([0], 1)
+    b = []
+    while n != 0:
+        b.append(n % 2)
+        n = n // 2 
+    b.reverse
+    return (b, len(b))
+
+print(conv_bin(9))
+
+
 def tri_bulles(T):
     n = len(T)
-    for i in range(...,...,-1):
+    for i in range(n-1,0,-1):
         for j in range(i):
-            if T[j] > T[...]:
-                ... = T[j]
-                T[j] = T[...]
+            if T[j] > T[j+1]:
+                temp = T[j]
+                T[j] = T[j+1]
                 T[j+1] = temp
     return T
