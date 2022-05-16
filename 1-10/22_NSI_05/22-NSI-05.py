6f189870234a32ca32e1b8a55ae7648812fe60fa
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-16 21:20:56 +0200

    Correction #5 manque de précision sur énoncé

--- 1-10/22_NSI_05/22-NSI-05.py
+++ 1-10/22_NSI_05/22-NSI-05.py
@@ -1,3 +1,19 @@
+def RechercheMinMax(tab):
+    res = {"min": None, "max": None}
+    if tab != []:
+        res["min"] = tab[0]
+        res["max"] = tab[0]
+    for i in tab: 
+        if i >= res["max"]:
+            res["max"] = i
+        else:
+            res["min"] = i 
+    return res 
+
+tableau = [0, 1, 4, 2, -2, 9, 3, 1, 7, 1] 
+print(RechercheMinMax(tableau))
+        
+
 class Carte:
     """Initialise Couleur (entre 1 à 4), et Valeur (entre 1 à 13)"""
     def __init__(self, c, v):
@@ -28,8 +44,12 @@ class PaquetDeCarte:
 
     """Remplit le paquet de cartes"""
     def remplir(self):
+        for i in range(4): 
+            for k in range(13):
+                self.contenu.append(Carte(i,k))
 	#A compléter
 
     """Renvoie la Carte qui se trouve à la position donnée"""
     def getCarteAt(self, pos):
-        #A compléter
+        if pos > 0 and pos <= len(self.contenu):
+            return self.contenu[pos]
