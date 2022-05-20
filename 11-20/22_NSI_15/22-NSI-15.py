cb92646e20724034316272849f4c5f677e09e19d
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-20 22:07:25 +0200

    Correction #15

--- 11-20/22_NSI_15/22-NSI-15.py
+++ 11-20/22_NSI_15/22-NSI-15.py
@@ -1,7 +1,15 @@
+def nb_repetitions(elt, tab):
+    nb = 0 
+    for i in tab:
+        if i == elt:
+            nb += 1 
+    return nb 
+
+
 def binaire(a):
-    bin_a = str(...)
+    bin_a = str(a%2) #on fait déjà le premier chiffre 
     a = a // 2
-    while a ... :
-        bin_a = ...(a%2) + ...
-        a = ...
+    while a > 1 : #tant que a supérieur a 1 (donc diff de 0)
+        bin_a = str(a%2) + bin_a #on reconstitue la chaine de caractere 
+        a = a // 2 #le nb est divisé par 2 strictement 
     return bin_a
