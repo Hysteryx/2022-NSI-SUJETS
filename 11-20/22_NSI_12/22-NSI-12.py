9a88173fb0db08859e3d37fb8a74d3fc665f880d
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-20 20:30:09 +0200

    Correction #12

--- 11-20/22_NSI_12/22-NSI-12.py
+++ 11-20/22_NSI_12/22-NSI-12.py
@@ -1,14 +1,22 @@
+def moyenne(tab):
+    if tab == []:
+        return 'Erreur'
+    prov = 0 
+    for i in tab:
+        prov += i 
+    return prov/len(tab)
+
 def tri(tab):
     #i est le premier indice de la zone non triee, j le dernier indice. 
     #Au debut, la zone non triee est le tableau entier.
-    i= ...
-    j= ...
+    i= 0
+    j= len(tab)
     while i != j :
         if tab[i]== 0:
-            i= ...
+            i= i + 1 
         else :
             valeur = tab[j]
-            tab[j] = ...
-            ...
-            j= ...
-    ...
+            tab[j] = tab[i]
+            tab[i] = valeur 
+            j= j - 1 
+    return tab 
