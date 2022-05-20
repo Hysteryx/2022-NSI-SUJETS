d17ba1a669bf4a8ba1850ba883dddc3f2b743ca7
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-21 00:00:26 +0200

    Correction #18

--- 11-20/22_NSI_18/22-NSI-18.py
+++ 11-20/22_NSI_18/22-NSI-18.py
@@ -1,13 +1,24 @@
+def mini(t_moy, annees):
+    min = (t_moy[0], annees[0])
+    for i in range(1,len(t_moy)):
+        if min[0] > t_moy[i]:
+            min = (t_moy[i], annees[i])
+    return min 
+
+t_moy = [14.9, 13.3, 13.1, 12.5, 13.0, 13.6, 13.7] 
+annees = [2013, 2014, 2015, 2016, 2017, 2018, 2019]
+print(mini(t_moy, annees))
+
 def inverse_chaine(chaine):
-    result = ...
+    result = ""
     for caractere in chaine:
-       result = ...
+       result = caractere + result 
     return result
 
 def est_palindrome(chaine):
     inverse = inverse_chaine(chaine)
-    return ...
+    return chaine == inverse #renvoie boolean 
     
 def est_nbre_palindrome(nbre):
-    chaine = ...
+    chaine = str(nbre)
     return est_palindrome(chaine)
