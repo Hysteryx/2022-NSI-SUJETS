803c371b71a823d55ce48cf94e153855353ec9a1
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-21 00:09:31 +0200

    Correction #19

--- 11-20/22_NSI_19/22-NSI-19.py
+++ 11-20/22_NSI_19/22-NSI-19.py
@@ -1,13 +1,26 @@
+def multiplication(n1, n2):
+    if n1 < 0:
+        return -multiplication(-n1, n2)
+    if n2 < 0:
+        return -multiplication(n1, -n2)
+    resultat = 0
+    for _ in range(n2):
+        resultat += n1
+    return resultat
+
+print(multiplication(-2,6))
+
+
 def chercher(T,n,i,j):
-    if i < 0 or ??? :
+    if i < 0 or j >= len(T) :
         print("Erreur")
         return None    
     if i > j :
         return None
-    m = (i+j) // ???
-    if T[m] < ??? :
-        return chercher(T, n, ??? , ???)
-    elif ??? :
-        return chercher(T, n, ??? , ??? )
+    m = (i+j) // 2
+    if T[m] < n :
+        return chercher(T, n, m + 1 , j)
+    elif T[m] > n :
+        return chercher(T, n, i , m-1 )
     else :
-        return ??? 
+        return m  
