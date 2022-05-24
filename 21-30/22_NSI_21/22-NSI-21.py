02a3b928ac0df67cec62a4b485c6feaf318371ed
Hysteryx <73034982+Hysteryx@users.noreply.github.com>
2022-05-24 11:13:47 +0200

    Correction #21

--- 21-30/22_NSI_21/22-NSI-21.py
+++ 21-30/22_NSI_21/22-NSI-21.py
@@ -1,3 +1,15 @@
+def multiplication(n1, n2):
+    if n1 < 0:
+        return -multiplication(-n1, n2)
+    if n2 < 0:
+        return -multiplication(n1, -n2)
+    res = 0 
+    for i in range(n1):
+        res += n2 
+    return res 
+
+print(multiplication(-2,6))
+
 def dichotomie(tab, x):
     """
         tab : tableau d’entiers trié dans l’ordre croissant
@@ -8,11 +20,14 @@ def dichotomie(tab, x):
     debut = 0 
     fin = len(tab) - 1
     while debut <= fin:
-        m = ...
+        m = fin-debut 
         if x == tab[m]:
-            return ...
+            return True
         if x > tab[m]:
             debut = m + 1
         else:
-             fin = ...			
-    return ...
+             fin = m - 1		
+    return False
+
+print(dichotomie([15, 16, 18, 19, 23, 24, 28, 29, 31, 33],27))
+
